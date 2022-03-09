701afb728972829bfae3fa5925bd3be3dc9afc36
Grimur <A75987687z>
2022-03-09 11:02:38 +0300

    fix installed apps error

--- mcpsmanager/mcpsmanager/settings.py
+++ mcpsmanager/mcpsmanager/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'helpdesk.apps.HelpdeskConfig',
     'teamsevent',
-    'botcollection.apps.BotcollectionConfig'
+    'botcollection.apps.BotcollectionConfig',
     'corsheaders',
     'eventtimeline.apps.EventTimelineConfig',
 ]
